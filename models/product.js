b3dcf9dd90ffc1d6c4ea11d4fca304050425c665
Manaswita Maiti <manaswitamaiti2002@gmail.com>
2022-07-19 16:18:35 +0530

    added cart and other routes, improved product model, edit and deleting products

--- models/product.js
+++ models/product.js
@@ -18,11 +18,15 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
