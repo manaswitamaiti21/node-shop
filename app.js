2d512411fff4e9087b0e29cc2a5cf97d66baf29e
Manaswita Maiti <manaswitamaiti2002@gmail.com>
2022-07-26 03:03:21 +0530

    Added signup and sign in functionality, hashing passwords using bcryptjs and protecting routes using the auth middleware

--- app.js
+++ app.js
@@ -57,18 +57,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Ritu',
-          email: 'ritu@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
