35ae7287d0c532ad8031ce006a0049b66746af24
Manaswita Maiti <manaswitamaiti2002@gmail.com>
2022-07-26 02:24:43 +0530

    Add a signup page

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
 exports.postLogin = (req, res, next) => {
   User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
@@ -20,6 +28,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch(err => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
